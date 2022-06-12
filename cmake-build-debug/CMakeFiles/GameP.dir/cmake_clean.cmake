5ef494f09db793fe26a37599e24c2d6fb2c0261e
zachsimop <zachsimop@gmail.com>
2022-06-12 13:35:53 -0500

    pushing init files

---
+++ cmake-build-debug/CMakeFiles/GameP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameP.dir/main.cpp.o"
+  "GameP"
+  "GameP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
